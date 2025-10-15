c5c15f0fc140a4e81f9034773e1c682675ed0ca9
siddwaji <siddwaji@sheridancollege.ca>
2025-10-14 20:36:14 -0400

    Added discount feature by Wajiha Siddiqui

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -2,4 +2,8 @@ public class ProductManager {
        public static void main(String[] args) {
            System.out.println("Product Manager System");
        }
+       
+       public double calculateDiscount(double price, double discountPercent) {
+           return price - (price * discountPercent / 100);
+       }
    }
