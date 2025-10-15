c538235e57088a6bbfbad5b20bf54026ed4062da
siddwaji <siddwaji@sheridancollege.ca>
2025-10-14 20:37:10 -0400

    Added documentation by Wajiha Siddiqui

--- src/ProductManager.java
+++ src/ProductManager.java
@@ -3,6 +3,7 @@ public class ProductManager {
            System.out.println("Product Manager System");
        }
        
+       // Calculates the final price after applying a discount percentage
        public double calculateDiscount(double price, double discountPercent) {
            return price - (price * discountPercent / 100);
        }
